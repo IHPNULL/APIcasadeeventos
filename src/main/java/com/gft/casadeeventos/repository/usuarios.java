0b76420908c0e9b6c1498c44456c07a82a27a588
italo <italo.haaspascoli@gmail.com>
2020-03-06 09:45:14 -0300

    Fim do mvp agora sim

--- src/main/java/com/gft/casadeeventos/repository/usuarios.java
+++ src/main/java/com/gft/casadeeventos/repository/usuarios.java
@@ -1,13 +1,11 @@
 package com.gft.casadeeventos.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gft.casadeeventos.model.Usuario;
 
-public interface usuarios extends  JpaRepository<Usuario, Long>{
-   
-	//Optional<Usuario> findByUsername(String username);
+public interface usuarios extends JpaRepository<Usuario, Long> {
+
+	// Optional<Usuario> findByUsername(String username);
 
 }

8d9d53c3578742601c62cc5390e95ec53c0479dc
IHPNULL <italo.haaspascoli@gmail.com>
2020-08-25 22:17:49 -0300

    fiz

--- src/main/java/com/gft/casadeeventos/services/UsuarioService.java
+++ src/main/java/com/gft/casadeeventos/services/UsuarioService.java
@@ -37,4 +37,22 @@ public class UsuarioService {
 			throw new CasaNaoEncontradaException("O usuário não foi encontrada!");
 		}
 	}
+
+	public void salvar(Long id) {
+		try {
+			usuRepo.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new CasaNaoEncontradaException("O usuário não foi encontrada!");
+		}
+	}
+
+	public void salvar(Long id) {
+		if (id != null) {
+			Optional<Usuario> a = usuRepo.findById(id);
+			if (a.isPresent()) {
+				throw new UsuarioExistenteException("Usuario já existe!");
+			}
+		}
+		return usuRepo.save(usuRepo.findById(id););
+	}
 }
